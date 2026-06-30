49a8d1dbacefda21f74949c74ea090589aeac273
Haydn202 <haydnthomson@hotmail.com>
2026-06-30 11:57:44 -0500

    add solution for q1

---
+++ Question1/solution/TestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿namespace TestProject1;
+
+public class Tests
+{
+    [Test]
+    [TestCase(17, 4, new[] { 4, 8, 12, 16 })]
+    [TestCase(21, 7, new[] { 14 })]
+    [TestCase(5, 20, new[] { 10, 20 })]
+    [TestCase(3, 10, new[] { 6 })]
+    [TestCase(2, 2, new[] { 2 })]
+    public void Test1(int num1,  int num2, int[] expectedValidNumbers)
+    {
+        var valid = new ValidNumbers(num1, num2);
+        
+        Assert.That(valid.GetValidNumbers(), Is.EqualTo(expectedValidNumbers));
+    }
+}
---
+++ Question1/solution/solution/Program.cs
@@ -0,0 +1,44 @@
+﻿void Main(string[] args)
+{
+    
+}
+
+public class ValidNumbers(int num1, int num2)
+{
+    private readonly List<int> _validNumbers = GetValidNumbers(num1, num2);
+
+    public List<int> GetValidNumbers()
+    {
+        return _validNumbers;
+    }
+
+    private void PrintValidNumbers()
+    {
+        foreach (var num in _validNumbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+    
+    private static List<int> GetValidNumbers(int num1, int num2)
+    {
+        var upperBound = num1 > num2 ? num1 : num2;
+        var divisor = num1 < num2 ? num1 : num2;
+        var validNumbers = new List<int>();
+
+        for (var i = divisor; i <= upperBound; i += divisor)
+        {
+            if (IsEven(i))
+            {
+                validNumbers.Add(i);
+            }
+        }
+
+        return validNumbers;
+    }
+
+    private static bool IsEven(int num)
+    {
+        return num % 2 == 0;
+    }
+}
